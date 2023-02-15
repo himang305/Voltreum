4fabc7c095d9bd747080f61e9423107ad2253075
HIMANSHU GAUTAM <himang305@gmail.com>
2023-02-15 12:24:16 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,13 +1,22 @@
+const {
+    expectEvent, // Assertions for emitted events
+    time,
+    expectRevert,
+} = require("@openzeppelin/test-helpers");
+
+
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 
 describe("Crypto_ICO_testing", function () {
-    return;
+
     let voltContract;
     let voltAddress;
     let icoContract;
     let icoAddress;
+    let gusdContract;
+    let gusdAddress;
     let vestingContract;
     let vestingAddress;
     let timelockContract;
@@ -19,87 +28,183 @@ describe("Crypto_ICO_testing", function () {
     let treasuryWallet;
 
     beforeEach(async function () {
-
-        [owner, invest1, invest2, treasuryWallet] = await ethers.getSigners();
+        console.log("Initializing");
+        [owner, invest1, treasuryWallet] = await ethers.getSigners();
         console.log("Active Address:" + owner.address);
+
+        const GUSD = await ethers.getContractFactory("GUSDToken");
+        gusdContract = await GUSD.deploy();
+        await gusdContract.deployed();
+        gusdAddress = gusdContract.address;
 
         const volts = await ethers.getContractFactory("Volt");
         voltContract = await volts.deploy(treasuryWallet.address);
         await voltContract.deployed();
         voltAddress = voltContract.address;
-        console.log(555);
 
+        const Timelock = await ethers.getContractFactory("voltreum_Vesting");
+        timelockContract = await Timelock.deploy(voltAddress);
+        await timelockContract.deployed();
+        timelockAddress = timelockContract.address;
 
         
-        // const Timelock = await ethers.getContractFactory("TimeLock");
-        // timelockContract = await Timelock.deploy(voltAddress);
-        // await timelockContract.deployed();
-        // timelockAddress = timelockContract.address;
+        const ICO = await ethers.getContractFactory("voltreum_ICO");
+        icoContract = await ICO.deploy( voltAddress, gusdAddress, timelockAddress, treasuryWallet.address );
+        await icoContract.deployed();
+        icoAddress = icoContract.address;
+        console.log("0.1");
 
-        // console.log(313);
-        // const ICO = await ethers.getContractFactory("ICO");
-        // icoContract = await ICO.deploy( voltAddress, timelockAddress);
-        // await icoContract.deployed();
-        // icoAddress = icoContract.address;
-
-        // var giveMinterRoleToIco = await voltContract.grantRole("0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6", icoAddress );
-        // console.log("Done prereq");
-
-        // var addLockerICO = await timelockContract.changeLockers(icoAddress);
 
     })
 
-    it("ICO Tests : ", async function () {
+    it("adding GUSD balance to user and iniitiate mint role",async function (){
+        await gusdContract.connect(invest1).mint(invest1.address,"1000000000000000000000000");
+        await gusdContract.connect(invest1).approve(icoAddress,"1000000000000000000000000");
+        console.log("1");
 
-
-        await voltContract.mint(invest1.address, ethers.utils.parseUnits("100","ether")) 
+        await icoContract.connect(owner).changeSaleStatus(true);
+        await voltContract.connect(owner).mint(icoContract.address,"50000000000000000000000000")
+        await timelockContract.connect(owner).setICOAddress(icoAddress);
+        await icoContract.connect(invest1).buyVoltFromNative(invest1.address,"1000000000000000000000000") 
+        console.log("2");
         
-        console.log("Supply : ", await voltContract.totalSupply());
-        console.log("balance of investor 1 sh be 100 eth : ", await voltContract.balanceOf(invest1.address));
+        let val = []
+        val = await timelockContract.userDataIdView(invest1.address);
+        console.log("entry", Number(val[0]));
+        console.log("3");
 
-        await voltContract.connect(invest1).transfer(invest2.address, ethers.utils.parseUnits("50","ether")) 
+        await time.increase(time.duration.days(30));
+        await timelockContract.connect(invest1).releaseTokens(1);
+        console.log("user balance after 30 days : ",  await voltContract.balanceOf(invest1.address));
+        
+         [a,
+            b,
+            c,
+            amount,
+            releasedAmount,
+            unlockCount,
+            d,e, withdrawcount] = await timelockContract.userData(1);
+        console.log("30 days : ", ethers.utils.formatEther(releasedAmount));
+        console.log("30 days : ", await timelockContract.userData(1));
 
-        console.log("balance of investor 2 sh be 50 eth : ", await voltContract.balanceOf(invest2.address));
+        console.log("4");
 
-        await voltContract.changeFeeStatus(true, 5);
+        await time.increase(time.duration.days(35));
+        await timelockContract.connect(invest1).releaseTokens(1);
+        console.log("user balance after 65 days : ",  await voltContract.balanceOf(invest1.address));
+        console.log("65 days : ", await timelockContract.userData(1));
+         [a,
+            b,
+            c,
+            amount,
+            releasedAmount,
+            unlockCount,
+            d,e, withdrawcount] = await timelockContract.userData(1);
+        console.log("65 days : ", ethers.utils.formatEther(releasedAmount));
+        console.log("5");
 
-        await voltContract.connect(invest1).transfer(invest2.address, ethers.utils.parseUnits("50","ether")) 
-
-        console.log("balance of investor 1 sh be 0 eth : ", await voltContract.balanceOf(invest1.address));
-        console.log("balance of investor 2 sh be 0 eth : ", await voltContract.balanceOf(invest2.address));
-
-        console.log("balance of treasury 50 eth 5%: ", await voltContract.balanceOf(treasuryWallet.address));
-
-        await voltContract.mint(invest1.address, ethers.utils.parseUnits("1000000000","ether")) 
-console.log("etst- - - - - -");
-        await voltContract.mint(invest1.address, ethers.utils.parseUnits("400000000","ether")) 
-
-
-        // var openSale = await icoContract.changeSaleStatus(true);
-
-        // var buyTokenFromBnb = await icoContract.connect(invest1).buyVoltFromNative(invest1.address, { value: ethers.utils.parseUnits("12","ether") });
-        // await buyTokenFromBnb.wait();
-
-        // expect(await timelockContract.lockAmount(invest1.address)).to.equal(ethers.utils.parseUnits("12","ether"));
-
-        // expect(await voltContract.balanceOf(timelockContract.address)).to.equal(ethers.utils.parseUnits("12","ether"));
-
-        // var reserveWallet = await icoContract.reserveWallet();
-
-        // expect(await ethers.provider.getBalance(reserveWallet)).to.equal(ethers.utils.parseUnits("12","ether"));
-
-
-        // var withdraw = await timelockContract.connect(invest1).releaseTokens();
-
-        // console.log(await voltContract.balanceOf(invest1.address));
-        // console.log(await voltContract.balanceOf(timelockContract.address));
+        await time.increase(time.duration.days(25));
+        await timelockContract.connect(invest1).releaseTokens(1);
+        console.log("user balance after 90 days : ",  await voltContract.balanceOf(invest1.address));
+        console.log("6");
+        console.log("90 days : ", await timelockContract.userData(1));
+         [a,
+            b,
+            c,
+            amount,
+            releasedAmount,
+            unlockCount,
+            d,e, withdrawcount] = await timelockContract.userData(1);
+        console.log("90 days : ", ethers.utils.formatEther(releasedAmount));
 
 
+        await time.increase(time.duration.days(90));
+        await timelockContract.connect(invest1).releaseTokens(1);
+        console.log("user balance after 180 days : ",  await voltContract.balanceOf(invest1.address));
+        console.log("7");
+        console.log("180 days : ", await timelockContract.userData(1));
+         [a,
+            b,
+            c,
+            amount,
+            releasedAmount,
+            unlockCount,
+            d,e, withdrawcount] = await timelockContract.userData(1);
+        console.log("180 days : ", ethers.utils.formatEther(releasedAmount));
 
 
-    });
+        await time.increase(time.duration.days(185));
+        await timelockContract.connect(invest1).releaseTokens(1);
+
+        console.log("user balance after 365 days : ",  await voltContract.balanceOf(invest1.address));
+        console.log("8");
+        console.log("365 days : ", await timelockContract.userData(1));
+         [a,
+            b,
+            c,
+            amount,
+            releasedAmount,
+            unlockCount,
+            d,e, withdrawcount] = await timelockContract.userData(1);
+        console.log("365 days : ", ethers.utils.formatEther(releasedAmount));
+
+
+    })
+
+    // it("ICO Tests : ", async function () {
+
+
+    //     await voltContract.mint(invest1.address, ethers.utils.parseUnits("100","ether")) 
+        
+    //     console.log("Supply : ", await voltContract.totalSupply());
+    //     console.log("balance of investor 1 sh be 100 eth : ", await voltContract.balanceOf(invest1.address));
+
+    //     await voltContract.connect(invest1).transfer(invest2.address, ethers.utils.parseUnits("50","ether")) 
+
+    //     console.log("balance of investor 2 sh be 50 eth : ", await voltContract.balanceOf(invest2.address));
+
+    //     await voltContract.changeFeeStatus(true, 5);
+
+    //     await voltContract.connect(invest1).transfer(invest2.address, ethers.utils.parseUnits("50","ether")) 
+
+    //     console.log("balance of investor 1 sh be 0 eth : ", await voltContract.balanceOf(invest1.address));
+    //     console.log("balance of investor 2 sh be 0 eth : ", await voltContract.balanceOf(invest2.address));
+
+    //     console.log("balance of treasury 50 eth 5%: ", await voltContract.balanceOf(treasuryWallet.address));
+
+    //     await voltContract.mint(invest1.address, ethers.utils.parseUnits("1000000000","ether")) 
+    //     console.log("etst- - - - - -");
+    //     await voltContract.mint(invest1.address, ethers.utils.parseUnits("400000000","ether")) 
+
+
+    //     // var openSale = await icoContract.changeSaleStatus(true);
+
+    //     // var buyTokenFromBnb = await icoContract.connect(invest1).buyVoltFromNative(invest1.address, { value: ethers.utils.parseUnits("12","ether") });
+    //     // await buyTokenFromBnb.wait();
+
+    //     // expect(await timelockContract.lockAmount(invest1.address)).to.equal(ethers.utils.parseUnits("12","ether"));
+
+    //     // expect(await voltContract.balanceOf(timelockContract.address)).to.equal(ethers.utils.parseUnits("12","ether"));
+
+    //     // var reserveWallet = await icoContract.reserveWallet();
+
+    //     // expect(await ethers.provider.getBalance(reserveWallet)).to.equal(ethers.utils.parseUnits("12","ether"));
+
+
+    //     // var withdraw = await timelockContract.connect(invest1).releaseTokens();
+
+    //     // console.log(await voltContract.balanceOf(invest1.address));
+    //     // console.log(await voltContract.balanceOf(timelockContract.address));
+
+
+
+
+    // });
 
 });
+
+
+
 
 
 
